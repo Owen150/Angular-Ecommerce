92c9a0c3e37c3149c6f92deea3f24dd58223152b
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-06-01 05:44:01 +0300

    Updates

--- src/app/core/Interceptors/product.interceptor.ts
+++ src/app/core/Interceptors/product.interceptor.ts
@@ -1,13 +1,16 @@
 import {
   HttpInterceptor,
   HttpRequest,
-  HttpHandler,
-  HttpEvent
+  HttpHandler
 } from '@angular/common/http';
 
 export class ProductInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler){
-    console.log('Product Interceptor Called')
+    console.log('Product Interceptor Called');
     return next.handle(req);
+
+    // Modifying a HttpRequest Header
+    // const modifiedReq = req.clone({headers: req.headers.append('Product', 'abcxyz')});
+    // return next.handle(modifiedReq);
   }
 }
