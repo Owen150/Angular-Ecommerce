491a70998dc6d77992a307e23aef7ad8a9ca1f9b
unknown <michaeloduor50@gmail.com>
2025-05-20 23:33:41 +0300

    Quotation Document Initialized

--- src/app/features/submodules/templates/templates.service.ts
+++ src/app/features/submodules/templates/templates.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TemplatesService {
+  
+  constructor() {}
 
-  constructor() { }
-
-  getInvoiceTemplateData() {
-    return {
-      customerName: '',
-      invoiceDate: new Date().toISOString().split('T')[0],
-      items: [{ name: '', qty: 1, price: 0 }],
-    };
-  }
-
-  getReportTemplateData() {
-    return {
-      title: '',
-      author: '',
-      content: '',
-      dateCreated: new Date().toISOString(),
-    };
+  getTemplate(templateType: string): any {
+    switch (templateType) {
+      case 'invoice':
+        return {
+          type: 'invoice',
+          customerName: 'John Doe',
+          invoiceDate: '2025-05-19',
+          items: [
+            { name: 'Service A', qty: 2, price: 100 },
+            { name: 'Service B', qty: 1, price: 250 },
+          ],
+        };
+      case 'report':
+        return {
+          type: 'report',
+          title: 'Monthly Report',
+          author: 'Jane Doe',
+          content: 'This is the report content...',
+          dateCreated: '2025-05-01',
+        };
+      default:
+        return null;
+    }
   }
 }
--- src/app/features/submodules/templates/templates/templates.component.ts
+++ src/app/features/submodules/templates/templates/templates.component.ts
@@ -1,25 +1,45 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { TemplatesService } from '../templates.service';
 
+declare var html2pdf: any;
+
 @Component({
   selector: 'app-templates',
   templateUrl: './templates.component.html',
   styleUrls: ['./templates.component.css'],
 })
 export class TemplatesComponent {
-  @ViewChild('invoiceTemplate') invoiceTemplate!: TemplateRef<any>;
-  @ViewChild('reportTemplate') reportTemplate!: TemplateRef<any>;
+  @ViewChild('invoice', { static: true }) invoiceTemplate!: TemplateRef<any>;
+  @ViewChild('report', { static: true }) reportTemplate!: TemplateRef<any>;
 
-  selectedTemplate: TemplateRef<any> | null = null;
+  activeTemplate: TemplateRef<any> | null = null;
+  templateContext: any = { data: {} };
 
-  invoiceData: any;
-  reportData: any;
+  constructor(private templateService: TemplatesService) {}
 
-  constructor(private template: TemplatesService) {}
+  loadTemplate(type: string) {
+    const data = this.templateService.getTemplate(type);
+    this.templateContext = { data };
+    switch (type) {
+      case 'invoice':
+        this.activeTemplate = this.invoiceTemplate;
+        break;
+      case 'report':
+        this.activeTemplate = this.reportTemplate;
+        break;
+      default:
+        this.activeTemplate = null;
+    }
+  }
+
+  exportToPDF() {
+    const element = document.getElementById('template-render');
+    if (element) {
+      html2pdf().from(element).save(`${this.templateContext.data.type}-template.pdf`);
+    }
+  }
 
   ngOnInit() {
-    if (this.selectedTemplate == this.invoiceTemplate) {
-      this.invoiceData = this.template.getInvoiceTemplateData();
-    } else this.reportData = this.template.getReportTemplateData();
+    this.loadTemplate('invoice');
   }
 }
