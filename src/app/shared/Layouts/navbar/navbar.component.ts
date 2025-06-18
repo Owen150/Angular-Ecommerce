90da3ab4814130288617bd06d99a8ce9622f5998
Michael Owen Oduor <michaeloduor50@gmail.com>
2025-06-18 16:57:49 +0300

    Homepage Working

--- src/app/shared/Layouts/navbar/navbar.component.ts
+++ src/app/shared/Layouts/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/features/submodules/cart/services/cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  cartItemCount: any;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.getCartItemCount();
+  }
+
+  getCartItemCount() {
+    this.cartService.cartSubject.subscribe((cartItems: any) => {
+      this.cartItemCount = cartItems.length;
+    });
+  }
 }
